60a9edd3b79b6c8fd36ef62be323eb8fdeb09a3d
Rodrigo Cruz <rodrigo.cruz76@gmail.com>
2018-03-16 10:31:44 -0300

    date default timezone

--- tupi.inicializar.php
+++ tupi.inicializar.php
@@ -1,7 +1,9 @@
 <?php
 //iniciando acessão
 session_start();
-
+date_default_timezone_set('America/Sao_Paulo');
+setlocale(LC_ALL, 'pt_BR', 'pt_BR.iso-8859-1', 'pt_BR.iso-8859-1', 'portuguese');
+header('Content-Type: text/html; charset=iso-8859-1');
 //incluindo todas as classes
 require("class/ready.php");
 $tupi = new Tupi();
