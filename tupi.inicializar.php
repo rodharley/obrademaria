38255fe8966336cda838f4f402220dc37bdbaeca
Rodrigo Cruz <rodrigo.cruz76@gmail.com>
2018-08-21 11:22:18 -0300

    adicionei composer e monolog

--- tupi.inicializar.php
+++ tupi.inicializar.php
@@ -6,6 +6,7 @@ setlocale(LC_ALL, 'pt_BR', 'pt_BR.iso-8859-1', 'pt_BR.iso-8859-1', 'portuguese')
 header('Content-Type: text/html; charset=iso-8859-1');
 //incluindo todas as classes
 require("class/ready.php");
+require("vendor/autoload.php");
 $tupi = new Tupi();
 $tupi->abrirConexao();
 
