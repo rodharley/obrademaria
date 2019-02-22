e88f64763b670b119f8848c84d00cc2f8792b087
Rodrigo Cruz <rodrigo.cruz76@gmail.com>
2019-02-22 09:58:37 -0300

    Acento no json do contratos em nuvem

--- class/modelo.Participante.php
+++ class/modelo.Participante.php
@@ -251,38 +251,36 @@ class Participante extends Persistencia{
 		$data = array("identidicadorLayout"=>$layout,
 			"numeroControleEmpresa"=>$this->id,
 			"documentoCliente"=>$oCliente->cpf,
-			"nomeCliente"=>$oCliente->nomeCompleto,
+			"nomeCliente"=>utf8_encode($oCliente->nomeCompleto),
 			"emailCliente"=>$oCliente->email,
 			"variaveis"=> array(
-				array("nome"=>"nomeCompleto","valor"=>$oCliente->nomeCompleto),
+				array("nome"=>"nomeCompleto","valor"=>utf8_encode($oCliente->nomeCompleto)),
 				array("nome"=>"estado_civil","valor"=>$oCliente->estadoCivil->descricao),
 				array("nome"=>"rg","valor"=>$oCliente->rg),
 				array("nome"=>"rgOrgaoExpedidor","valor"=>$oCliente->orgaoEmissorRg),
 				array("nome"=>"cpf","valor"=>$oCliente->cpf),
-				array("nome"=>"endereco","valor"=>$oCliente->endereco),
-				array("nome"=>"cidade","valor"=>$oCliente->cidadeEndereco),
+				array("nome"=>"endereco","valor"=>utf8_encode($oCliente->endereco)),
+				array("nome"=>"cidade","valor"=>utf8_encode($oCliente->cidadeEndereco)),
 				array("nome"=>"uf","valor"=>$oCliente->estadoNascimento),
 				array("nome"=>"nacionalidade","valor"=>$oCliente->nacionalidade),				
 				array("nome"=>"taxaAdesao","valor"=>$taxaAdesao),
 				array("nome"=>"CIFRAO","valor"=>$oGrupo->moeda->cifrao),
 				array("nome"=>"total","valor"=>$this->money($this->valorTotal,"atb")),
 				array("nome"=>"totalPassagem","valor"=>$_REQUEST['valorPassagem']),
-				array("nome"=>"nometestemunha1","valor"=>$_REQUEST['nometestemunha1']),
-				array("nome"=>"nometestemunha2","valor"=>$_REQUEST['nometestemunha2']),
+				array("nome"=>"nometestemunha1","valor"=>utf8_encode($_REQUEST['nometestemunha1'])),
+				array("nome"=>"nometestemunha2","valor"=>utf8_encode($_REQUEST['nometestemunha2'])),
 				array("nome"=>"rgtestemunha1","valor"=>$_REQUEST['rgtestemunha1']),
 				array("nome"=>"rgtestemunha2","valor"=>$_REQUEST['rgtestemunha2']),
 				array("nome"=>"dia","valor"=>date("d")),
-				array("nome"=>"mes","valor"=>$this->mesExtenso(date("m"))),
+				array("nome"=>"mes","valor"=>utf8_encode($this->mesExtenso(date("m")))),
 				array("nome"=>"ano","valor"=>date("Y"))));				
 			
-				
-			
-
-
+	
 		$ret = $this->loginContratosEmnuvem();
 		$headers = array('Accept' => 'application/json','X-Token'=>$ret->jwt,'Content-Type'=>'application/json; charset=utf-8');
 		$query = Unirest\Request\Body::json($data);
-		 $response = Unirest\Request::post($this->endpointcn.'documentos/criar-documento', $headers, $query);
+	
+		$response = Unirest\Request::post($this->endpointcn.'documentos/criar-documento', $headers, $query);
 		 $oLog = new LogUsuario();
 		 $user = new Usuario();
 		$user->id = $_SESSION['ag_idUsuario'];
