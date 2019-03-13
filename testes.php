9bbad02fb8278c7dd07c652fa8dc8bbae98ee2b3
Rodrigo Cruz <rodrigo.cruz76@gmail.com>
2019-03-13 13:50:50 -0300

    testes

--- testes.php
+++ testes.php
@@ -1,2 +1,6 @@
 <?php
-print_r(localeconv());
+include("tupi.inicializar.php");
+ $headers = array('Accept' => 'application/json','Token' => base64_encode("sistema@obrademaria.com.br:$%VSDGS#g%hrdvB"));
+ Unirest\Request::verifyPeer(false); 
+ $response = Unirest\Request::get('https://contratosemnuvem.com.br/servicos/api/index.php/free/auth', $headers, null);
+ echo $response->body->jwt;
