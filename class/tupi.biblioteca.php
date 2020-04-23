03a247b349a76c2cded8987bf55e39ee460f566f
Rodrigo Cruz <rodrigo.cruz76@gmail.com>
2020-04-22 23:05:36 -0300

    debug modo

--- class/tupi.biblioteca.php
+++ class/tupi.biblioteca.php
@@ -21,7 +21,7 @@ class Biblioteca {
 	var $endpointcn = 'http://obrademariadf.com.br/mydocs/api/index.php/';
 	var $usercn = "sistema@obrademaria.com.br";
 	var $senhacn = "$%VSDGS#g%hrdvB";
-	
+	var $debug = false;
 	
 
 	
