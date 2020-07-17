2ce84b4ea12ffd5eda04b8216603718cf63bd37b
00007311966 <rodrigo.camilo-cruz@serpro.gov.br>
2020-07-17 18:08:16 -0300

    cielo

--- demandas/ecommerce_sql.sql
+++ demandas/ecommerce_sql.sql
@@ -86,3 +86,65 @@ INSERT INTO `ag_menu` (`id`, `idMenuPai`, `descricao`, `url`) VALUES ('', '2', '
 --11/07/2020
 ALTER TABLE `ag_grupo` ADD `desconto_avista` INT(3) NOT NULL DEFAULT '0' AFTER `imagem_destaque`; 
 ALTER TABLE `ag_venda_site` ADD `desconto` INT(3) NOT NULL DEFAULT '0' AFTER `total`; 
+--17/07/20202
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 17-Jul-2020 às 21:06
+-- Versão do servidor: 8.0.18
+-- versão do PHP: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `obrademariadf1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `ag_checkout_cielo`
+--
+
+DROP TABLE IF EXISTS `ag_checkout_cielo`;
+CREATE TABLE IF NOT EXISTS `ag_checkout_cielo` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `checkout_url` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
+  `profile` varchar(30) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
+  `version` int(4) DEFAULT NULL,
+  `venda` bigint(20) NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  `created_date` datetime NOT NULL,
+  `checkout_cielo_order_number` varchar(32) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
+  `payment_method_type` tinyint(4) DEFAULT NULL,
+  `payment_method_brand` int(11) DEFAULT NULL,
+  `payment_method_bank` int(11) DEFAULT NULL,
+  `payment_maskedcreditcard` varchar(20) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
+  `payment_installments` int(11) DEFAULT NULL,
+  `payment_status` int(11) DEFAULT NULL,
+  `tid` varchar(20) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
+  `test_transaction` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_cielo_venda` (`venda`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+
+
+ALTER TABLE `ag_checkout_cielo`
+  ADD CONSTRAINT `fk_cielo_venda` FOREIGN KEY (`venda`) REFERENCES `ag_venda_site` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
