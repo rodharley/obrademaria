6bc95a6ca67b102891d83b558a90b05932328b24
00007311966 <rodrigo.camilo-cruz@serpro.gov.br>
2020-07-16 21:54:34 -0300

    aniversariantes

--- scripts/aniversariantes.php
+++ scripts/aniversariantes.php
@@ -3,13 +3,13 @@
 include("../tupi.inicializar.php");
             
 $dia = date("Y-m-d");
-$sql  ="SELECT * FROM ag_cliente where month(dataNascimento) = month('".$dia."') and day(dataNascimento) = day('".$dia."') and email != ''";
+$sql  ="SELECT * FROM ag_cliente where month(dataNascimento) = month('".$dia."') and day(dataNascimento) = day('".$dia."')";
 echo "<CENTER><H2>ANIVERSSARIANTES DO DIA</H2></CENTER><BR/>";
 $obCliente = new Cliente();
 $rs = $obCliente->getSQL($sql);
+echo    "NOME;EMAIL;CELULAR<br/>";
 foreach ($rs as $key => $value) {
     # code...
-echo    "NOME;EMAIL;CELULAR<br/>";
     echo $value->nomeCompleto.";".$value->email.";".$value->celular."<br/>";
 }
 ?>
