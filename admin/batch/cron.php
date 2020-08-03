c8f6457e32ef2be285649dee109b25d782e20462
00007311966 <rodrigo.camilo-cruz@serpro.gov.br>
2020-08-03 10:34:51 -0300

    cron

--- admin/batch/cron.php
+++ admin/batch/cron.php
@@ -1,5 +1,22 @@
 <?php
-include(str_replace("batch/cron.php","",$_SERVER['SCRIPT_FILENAME'])."tupi.inicializar.php");
+$path = str_replace("batch/cron.php","",$_SERVER['SCRIPT_FILENAME']);
+//incluindo todas as classes
+include($path."class/ready.php");
+include($path."vendor/autoload.php");
+include($path."vendor/drupalista-br/checkoutcielo-library/src/Checkout/Commons.php");
+include($path."vendor/drupalista-br/checkoutcielo-library/src/Checkout/Address.php");
+include($path."vendor/drupalista-br/checkoutcielo-library/src/Checkout/Cart.php");
+include($path."vendor/drupalista-br/checkoutcielo-library/src/Checkout/Customer.php");
+include($path."vendor/drupalista-br/checkoutcielo-library/src/Checkout/Discount.php");
+include($path."vendor/drupalista-br/checkoutcielo-library/src/Checkout/Item.php");
+include($path."vendor/drupalista-br/checkoutcielo-library/src/Checkout/Options.php");
+include($path."vendor/drupalista-br/checkoutcielo-library/src/Checkout/Order.php");
+include($path."vendor/drupalista-br/checkoutcielo-library/src/Checkout/Payment.php");
+include($path."vendor/drupalista-br/checkoutcielo-library/src/Checkout/Services.php");
+include($path."vendor/drupalista-br/checkoutcielo-library/src/Checkout/Shipping.php");
+include($path."vendor/drupalista-br/checkoutcielo-library/src/Checkout/Transaction.php");
+$menusite = 0;
+$tupi = new Tupi();
 try{
     //executa agendamentos
 	$ag = new Agendamento();
