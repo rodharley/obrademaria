390472aa26cd2e8a1e308ef099ff048fb3b97968
00007311966 <rodrigo.camilo-cruz@serpro.gov.br>
2020-08-15 11:12:35 -0300

    email corrigido

--- ajax/sentEmail.php
+++ ajax/sentEmail.php
@@ -2,19 +2,19 @@
 header("Content-Type: text/html; charset=iso-8859-1");
 include("../admin/tupi.inicializar.php"); 
 try{
-    if(isset($_SESSION['userIpemail'])){
+    /*if(isset($_SESSION['userIpemail'])){
         if($_SERVER['REMOTE_ADDR'] == $_SESSION['userIpemail']){
             throw new Exception("Você já enviou");
         }
-    }
+    }*/
 if(isset($_REQUEST['grupo'])){
-$mensagem = 'Grupo: '.$_REQUEST['grupo'].'<br/>'.'Nome: '.$_REQUEST['nome'].'<br/>'.'Mensagem: '.$_REQUEST['message'];
+$mensagem = 'Grupo: '.$_REQUEST['grupo'].'<br/>'.'Nome: '.$_REQUEST['nome'].'<br/>'.'Email: '.$_REQUEST['email'].'<br/>'.'Mensagem: '.$_REQUEST['message'];
 }else{
-$mensagem = 'Mensagem: '.$_REQUEST['message'];    
+$mensagem = 'Email: '.$_REQUEST['email'].'<br/>'.'Mensagem: '.$_REQUEST['message'];    
 }
 
-if($tupi->mail_html($tupi->DESTINATARIO,$_REQUEST['email'], 'Email do Site', $mensagem)){
-    $_SESSION['userIpemail'] = $_SERVER['REMOTE_ADDR'];
+if($tupi->mail_html($tupi->DESTINATARIO,$tupi->REMETENTE, 'Email do Site', $mensagem)){
+    //$_SESSION['userIpemail'] = $_SERVER['REMOTE_ADDR'];
     echo json_encode(array("code"=>"200","data"=>array("mensagem"=>utf8_encode("Email Enviado com sucesso!"))));
 }else{
     echo json_encode(array("code"=>"500","data"=>array("mensagem"=>utf8_encode("Não foi possível enviar o Email, tente novamente mais tarde"))));
